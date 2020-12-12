3bbe26271e983f7ef1ba3d3938631999dcfb19bc
Sean Nelson <snelson@redhat.com>
2020-12-12 16:29:24 -0700

    added manifest to install argocd v1.8.1

--- update-charts.sh
+++ update-charts.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
 
-for app in $(ls apps/); do
-    mkdir -p apps/${app}/base/{charts,manifests}
-    helm pull ${app}/${app} --untar --untardir apps/${app}/base/charts
+# for app in $(ls apps/); do
+#     mkdir -p apps/${app}/base/{charts,manifests}
+#     helm pull ${app}/${app} --untar --untardir apps/${app}/base/charts
+# done
+
+apps="jenkins/jenkins"
+
+for app in ${apps}
+do
+    app_repo=`dirname ${app}`
+    app_name=`basename ${app}`
+    dir_path="apps/${app_name}/base/charts"
+
+    helm pull ${app_repo}/${app_name} --untar --untardir ${dir_path}
 done
